0166125eca32326b084e43bb5fdc7d2aae7eaa35
Eric Stratton <strattonericj@gmail.com>
2020-12-22 15:37:25 -0800

    add Name property to Animal object
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -7,5 +7,9 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Type { get; set; }
+    public string Name { get; set; }
+    // public string DOA { get; set; }
+    // public string Gender { get; set; }
+    // public string Breed { get; set; }
   }
 }
