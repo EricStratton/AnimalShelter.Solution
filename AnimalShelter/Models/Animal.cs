cd1d4eebfff1e416e823c1cfbc4668b2a8492521
Eric Stratton <strattonericj@gmail.com>
2020-12-22 15:48:15 -0800

    add DOA, Breed, and Gender properties to Animal object
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -8,8 +8,8 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Type { get; set; }
     public string Name { get; set; }
-    // public string DOA { get; set; }
-    // public string Gender { get; set; }
-    // public string Breed { get; set; }
+    public string DOA { get; set; }
+    public string Gender { get; set; }
+    public string Breed { get; set; }
   }
 }
